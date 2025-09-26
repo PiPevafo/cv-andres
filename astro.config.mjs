e0414cf41b59e7777ad1bbcaf30e0d0c2350accc
Andres Valencia <andresfvalenciafonseca2010@gmail.com>
2025-09-25 21:25:04 -0500

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://niaggar.github.io/',
-	base: '/cv-page-template',
+	site: 'https://PiPevafo.github.io/',
+	base: '/cv-andres',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		remarkPlugins: [remarkMath],
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'NAME SURNAME';
+export const SITE_TITLE = 'Andres F. Valencia F.';
 export const SITE_DESCRIPTION = 'NAME SURNAME\'s personal website';
-export const BASE_URL = '/cv-page-template';
+export const BASE_URL = '/cv-andres';
